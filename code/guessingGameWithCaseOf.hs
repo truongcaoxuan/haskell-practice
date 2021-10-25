0e119c65f561345997b683e6e02e7a62c387c8f9
Truong Cao Xuan <54384725+truongcaoxuan@users.noreply.github.com>
2021-10-25 18:45:00 +0700

    Create guessingGameWithCaseOf.hs

---
+++ code/guessingGameWithCaseOf.hs
@@ -0,0 +1,22 @@
+module Main where
+import System.Random
+
+main :: IO ()
+main = do putStrLn "Try to guess the secret word ([1-100])..."
+          secret <- randomRIO (1, 100)
+          play secret
+
+play :: Int -> IO ()
+play secret = do guesses <- playGame secret 0
+                 putStrLn $ "You win! You took " ++ show guesses ++ " guesses!"
+ 
+playGame :: Int -> Int -> IO Int
+playGame secret guesses = do putStr "Enter your guess: "
+                             input <- getLine
+                             let guess = read input :: Int
+                             case compare guess secret of
+                               LT -> do putStrLn "Too low!"
+                                        playGame secret (guesses + 1)
+                               GT -> do putStrLn "Too high!"
+                                        playGame secret (guesses + 1)
+                               EQ -> do return (guesses + 1)
