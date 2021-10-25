4dd4fc131ba1732e04cf55c6e0dfe69047552693
Truong Cao Xuan <54384725+truongcaoxuan@users.noreply.github.com>
2021-10-25 18:58:21 +0700

    Update facFun.hs

--- code/function/facFun.hs
+++ code/function/facFun.hs
@@ -1,11 +1,16 @@
 --
-facAUX :: (Integral a) => a -> a
-facAUX n = aux n 1
+fac :: (Integral a) => a -> a
+fac n = aux n 1
     where
         aux n acc
            | n <=1 = acc
            | otherwise = aux (n-1) (n*acc)
 
+--
+fac :: (Integral a) => a -> a
+fac n
+    | n<=0 = 1
+    | otherwise = n * fac (n-1)
 --
 fac :: (Integral a) => a -> a
 fac 0 = 1
